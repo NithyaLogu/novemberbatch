c4f5b0890140b77acf49456bc28fbecb4c49f784
Nithya <153349668+NithyaLogu@users.noreply.github.com>
2024-01-04 15:11:20 +0530

    Add files via upload

---
+++ TodoListProject.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+<link rel="stylesheet"href="index.css"/> 
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1> TODO LIST</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Add new task"
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
